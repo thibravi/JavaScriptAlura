6555ff0732fbf654c4f22662f9016625de755764
Thiago Bravi <thiagobravi@gmail.com>
2022-11-17 09:39:29 -0300

    atualização

--- map-nomes-padronizacao.js
+++ map-nomes-padronizacao.js
@@ -12,13 +12,17 @@
 
 
 //aqui eu consegui colocar tudo em minúsculo e depois colocar a primeira letra em maiúsculo
-let nomes = ["ana Julia", "Caio vinicius", "BIA silva"]
+//seguindo com os testes e usando o .split agora
+let nomes = ["ana Julia", " Caio vinicius", " BIA silva"] //para funcionar eu precisei colocar espaço antes do segundo e terceiro nome. Ainda preciso resolver isso ainda.
 
 let nomesAtualizados = nomes.map(nome => nome.toLowerCase())
 
-for (let i = 0; i < nomesAtualizados.length; i++){
-    nomesAtualizados[i] = nomesAtualizados[i][0].toUpperCase() + nomesAtualizados[i].substring(1)
+let splitNames = nomesAtualizados.toString().split(" ")
+
+for (let i = 0; i < splitNames.length; i++){
+    splitNames[i] = splitNames[i][0].toUpperCase() + splitNames[i].substring(1)
 }
 
+let juntandoNomes = splitNames.join(" ")
 
-console.log(nomesAtualizados)
+console.log(juntandoNomes)
